39e70365d60cb4ab910607fbe957f30ce96c3cab
Vitoria Belshoff <103432976+Tenkoria@users.noreply.github.com>
2022-06-16 14:11:56 -0300

    Create script-QueryRecursiva.sql

---
+++ pset3/script-QueryRecursiva.sql
@@ -0,0 +1,28 @@
+
+WITH RECURSIVE tabela_pai AS
+( SELECT
+codigo, nome, codigo_pai,
+CAST(nome AS TEXT) AS nome_relacao
+FROM
+classificacao
+WHERE
+codigo_pai IS NULL
+UNION ALL
+SELECT
+c.codigo,
+tp.nome,
+c.codigo_pai,
+CAST(tp.nome_relacao || ' -> ' || c.nome AS TEXT) AS nome_relacao
+FROM
+classificacao c
+INNER JOIN
+tabela_pai tp ON c.codigo_pai = tp.codigo
+)
+SELECT
+nivel,
+nome_relacao,
+codigo_pai,
+FROM
+tabela_pai
+ORDER BY
+nome_relacao;
